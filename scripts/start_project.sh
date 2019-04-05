f5aaca908b07090f1500da0af5732e99b5cf49b1
Roberto Zegers <rzegers@uc.cl>
2019-04-05 10:47:28 +0000

    Fix bash script that caused the abrupt termination of the simulation

--- scripts/start_project.sh
+++ scripts/start_project.sh
@@ -12,11 +12,16 @@ xterm  -e "source devel/setup.bash; roslaunch rll_planning_project path_planner.
 sleep 4
 xterm  -e "source devel/setup.bash; roslaunch rll_project_runner run_project.launch" &
 
-# Termiante the project
-echo "Press Enter key to terminate the project"
-read -s -n 1 key
-if [[ $key = "" ]]; then 
-	killall -e xterm
-	echo "Project terminated!"
-fi
+## Termiante the project
+# echo "Press Enter key to terminate the project"
+## read reads a single line from standard input, 
+## -s Do not echo keystrokes when read is taking input from the terminal
+## -n nchars Stop reading after an integer number nchars characters have been read, if the line delimiter has not been reached.
+
+# read -s -n 1 key
+# if [[ $key = "" ]]; then 
+# 	killall -e xterm
+# 	echo "Project terminated!"
+## fi closes the if statement
+# fi
 

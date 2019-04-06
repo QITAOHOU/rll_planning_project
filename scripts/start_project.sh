81da0bba1f2a93bde3203b5ec4d812a1cfef47be
Roberto Zegers <rzegers@uc.cl>
2019-04-06 06:40:30 +0200

    Comment project launch script file

--- scripts/start_project.sh
+++ scripts/start_project.sh
@@ -3,13 +3,17 @@
 # Build the catkin_ws
 cd $(pwd)/../../.. ; catkin_make
 
-# Launch the nodes
+## Launch the nodes
+# Load Gazebo and the whole MoveIt! setup 
 xterm  -e "source devel/setup.bash; roslaunch rll_planning_project moveit_planning_execution.launch" &
 sleep 10
+# Set the start and goal pose, spawn grasp object, set if the path planner will be called 3 times
 xterm  -e "source devel/setup.bash; roslaunch rll_planning_project planning_iface.launch" &
 sleep 4	
+# Launch the path planing algorithm developed as solution to the project
 xterm  -e "source devel/setup.bash; roslaunch rll_planning_project path_planner.launch" &
 sleep 4
+# Send a goal, evaluate the result, reset robot and environment
 xterm  -e "source devel/setup.bash; roslaunch rll_project_runner run_project.launch" &
 
 ## Termiante the project
